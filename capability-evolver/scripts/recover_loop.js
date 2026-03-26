899711a500829335196f08e4ba23ca1743f0a565
雷云康 <leiyunkang7@outlook.com>
2026-03-26 14:06:35 +0800

    chore: 添加.gitignore + shebang改为bun

--- capability-evolver/scripts/recover_loop.js
+++ capability-evolver/scripts/recover_loop.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
--- evolver/index.js
+++ evolver/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 const evolve = require('./src/evolve');
 const { solidify } = require('./src/gep/solidify');
 const path = require('path');
